4d5b7c861036fabc43e709e151d8c398f2402101
Julien <julien.bouchard.it@gmail.com>
2019-11-27 18:36:25 -0500

    adding exit and ajusting duty

--- motor_control.py
+++ motor_control.py
@@ -5,6 +5,8 @@ in1 = 24
 in2 = 23
 en = 25
 pause = 1
+duty = 100  # Duty cycle, see: https://lastminuteengineers.com/l298n-dc-stepper-driver-arduino-tutorial/
+pause_demo = 1
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1, GPIO.OUT)
@@ -13,7 +15,7 @@ GPIO.setup(en, GPIO.OUT)
 GPIO.output(in1, GPIO.LOW)
 GPIO.output(in2, GPIO.LOW)
 p = GPIO.PWM(en, 1000)
-p.start(25)  # Duty cycle
+p.start(duty)
 p.ChangeDutyCycle(50)  # Speed, should be between 25 et 75 (50 = medium)
 
 
@@ -32,16 +34,21 @@ def backward():
     GPIO.output(in2, GPIO.HIGH)
 
 
+def exit():
+    GPIO.cleanup()
+
+
 def demo():
     print('Starting demo')
-    sleep(1)
+    sleep(pause_demo)
     print('Forward')
     forward()
-    sleep(1)
+    sleep(pause_demo)
     print('Backward')
-    sleep(1)
+    sleep(pause_demo)
     print('Stop')
     stop()
+    exit()
 
 
 if __name__ == '__main__':
