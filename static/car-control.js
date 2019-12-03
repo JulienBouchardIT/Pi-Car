fdbf61253cdfdda28694903219b0658f736a5d19
Julien <julien.bouchard.it@gmail.com>
2019-12-03 00:30:19 -0500

    Fix call api, power obj type

--- static/car-control.js
+++ static/car-control.js
@@ -1,30 +1,24 @@
 
 function send_command(power, command){
-    var form = new FormData();
-    form.append("command", command);
-    form.append("power", power);
-
     var settings = {
       "async": true,
       "crossDomain": true,
-      "url": "http://98.143.215.223/api/command",
+      "url": "/api/command",
       "method": "POST",
       "headers": {
-        "User-Agent": "PostmanRuntime/7.19.0",
+        "Content-Type": "application/x-www-form-urlencoded",
         "Accept": "*/*",
         "Cache-Control": "no-cache",
-        "Postman-Token": "fd09bab5-0983-4a82-8021-d614d42ece87,69c0e70a-3062-4223-9e13-1d9060c2797f",
-        "Host": "98.143.215.223",
-        "Content-Type": "multipart/form-data; boundary=--------------------------339503257207581453269352",
+        "Host": "192.168.0.102:80",
         "Accept-Encoding": "gzip, deflate",
-        "Content-Length": "273",
+        "Content-Length": "23",
         "Connection": "keep-alive",
         "cache-control": "no-cache"
       },
-      "processData": false,
-      "contentType": false,
-      "mimeType": "multipart/form-data",
-      "data": form
+      "data": {
+        "command": command,
+        "power": power
+      }
     }
 
     $.ajax(settings).done(function (response) {
